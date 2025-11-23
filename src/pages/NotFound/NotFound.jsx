0d1af2765f4bd0888a0a79bb6cd34d938de3a1b9
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-11-23 16:44:58 +0600

    feat(NotFoundPage): moving back button added to NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import notFound from '../../assets/images/not-found.png';
 import Container from '../../components/common/Container/Container';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import { FaArrowLeft } from 'react-icons/fa6';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <Container className=" bg-base-100 p-20 rounded-xl flex flex-col justify-center items-center gap-10">
+      <Container className="relative bg-base-100 p-20 rounded-xl flex flex-col justify-center items-center gap-10">
+        <div className="absolute top-5 left-5">
+          <button
+            className="flex justify-between items-center gap-2.5 px-5 py-2 text-[#606060] font-medium cursor-pointer hover:-translate-x-1.5 transition-all duration-400"
+            onClick={() => navigate(-1)}
+          >
+            {' '}
+            <span>
+              <FaArrowLeft />
+            </span>
+            Go Back
+          </button>
+        </div>
         <div>
           <img src={notFound} alt="Not Found" />
         </div>
